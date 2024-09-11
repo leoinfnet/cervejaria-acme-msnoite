6e55c18dbdfe563e48c3d61f6bfd9eeffbecd0a8
leonardogloria <contato@leonardogloria.com.br>
2024-09-11 17:56:41 -0300

    RElisiencia , Log Distribuido e Trace

---
+++ eureka/src/test/java/com/acme/eureka/EurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.acme.eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
